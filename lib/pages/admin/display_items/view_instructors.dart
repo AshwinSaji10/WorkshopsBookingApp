8487cb81de3e952256463f32cadad76cbad1deba
AshwinSaji10 <aswinsaji777@gmail.com>
2024-09-18 20:29:28 +0530

    created templates for viewing instructors and sessions

---
+++ lib/pages/admin/display_items/view_instructors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class ViewInstructors extends StatefulWidget {
+  const ViewInstructors({super.key});
+
+  @override
+  State<ViewInstructors> createState() => _ViewInstructorsState();
+}
+
+class _ViewInstructorsState extends State<ViewInstructors> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/pages/admin/display_items/view_sessions.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class ViewSessions extends StatefulWidget {
+  const ViewSessions({super.key});
+
+  @override
+  State<ViewSessions> createState() => _ViewSessionsState();
+}
+
+class _ViewSessionsState extends State<ViewSessions> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
