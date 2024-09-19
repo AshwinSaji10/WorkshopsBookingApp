161fbf638a0e4c04e67f3ab0736a7f7123057ab3
AshwinSaji10 <aswinsaji777@gmail.com>
2024-09-19 19:47:02 +0530

    added ability to view all instructors

--- lib/pages/admin/display_items/view_instructors.dart
+++ lib/pages/admin/display_items/view_instructors.dart
@@ -1,4 +1,6 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:workshops_booking/services/database_service.dart';
+import 'package:workshops_booking/models/instructors.dart';
 
 class ViewInstructors extends StatefulWidget {
   const ViewInstructors({super.key});
@@ -8,8 +10,101 @@ class ViewInstructors extends StatefulWidget {
 }
 
 class _ViewInstructorsState extends State<ViewInstructors> {
+  final DatabaseService _dbService = DatabaseService.instance;
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("List of Instructors"),
+      ),
+      body: FutureBuilder(
+        future: _dbService.getInstructors(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return const Center(child: Text("Error fetching instructors"));
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return const Center(child: Text("No instructors available"));
+          } else {
+            return ListView.builder(
+              itemCount: snapshot.data?.length ?? 0,
+              itemBuilder: (context, index) {
+                Instructors instructor = snapshot.data![index];
+
+                return Container(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                  margin:
+                      const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                  height: 100,
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                    color: Colors.blueAccent,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            children: [
+                              const Text("ID: "),
+                              Text(instructor.iid!),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              const Text("Name: "),
+                              Text(instructor.iname!),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              const Text("Gender: "),
+                              Text(instructor.gender!),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              const Text("Age: "),
+                              Text(instructor.age.toString()),
+                            ],
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          IconButton(
+                            onPressed: () {
+                              setState(
+                                () {
+                                  _dbService.deleteInstructor(instructor.iid!);
+                                },
+                              );
+                            },
+                            icon: const Icon(Icons.delete),
+                          ),
+                          IconButton(
+                            onPressed: () {
+                              // Navigate to an edit page or handle edit logic
+                            },
+                            icon: const Icon(Icons.edit),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                );
+              },
+            );
+          }
+        },
+      ),
+    );
   }
 }
