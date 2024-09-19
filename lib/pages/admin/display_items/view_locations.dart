744023f41409ec7178ac5492ae43212f624201bc
AshwinSaji10 <aswinsaji777@gmail.com>
2024-09-19 19:47:15 +0530

    added ability to view all locations

--- lib/pages/admin/display_items/view_locations.dart
+++ lib/pages/admin/display_items/view_locations.dart
@@ -1,6 +1,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:workshops_booking/services/database_service.dart';
+import 'package:workshops_booking/models/locations.dart';
 
 class ViewLocations extends StatefulWidget {
   const ViewLocations({super.key});
@@ -15,6 +16,89 @@ class _ViewLocationsState extends State<ViewLocations> {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("List of Locations"),
+      ),
+      body: FutureBuilder(
+        future: _dbService.getLocations(),
+        builder: (context, snapshot) {
+          return ListView.builder(
+            itemCount: snapshot.data?.length ?? 0,
+            itemBuilder: (context, index) {
+              Locations location = snapshot.data![index];
+              // return ListTile(
+              //   title: Text(workshop.wname!),
+              // );
+              return Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                margin:
+                    const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                height: 80,
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  color: Colors.blueAccent,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            const Text("id:"),
+                            Text(location.lid!),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            const Text("name:"),
+                            Text(location.lname!),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            const Text("capacity:"),
+                            Text(location.capacity.toString()),
+                          ],
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        IconButton(
+                          onPressed: () {
+                            setState(
+                              () {
+                                _dbService.deleteLocation(location.lid!);
+                              },
+                            );
+                          },
+                          icon: const Icon(Icons.delete),
+                        ),
+                        IconButton(
+                            onPressed: () {}, icon: const Icon(Icons.edit))
+                      ],
+                    )
+
+                    // Column(
+                    //   mainAxisAlignment: MainAxisAlignment.start,
+                    //   crossAxisAlignment: CrossAxisAlignment.start,
+                    //   children: [
+
+                    //   ],
+                    // )
+                  ],
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
   }
 }
