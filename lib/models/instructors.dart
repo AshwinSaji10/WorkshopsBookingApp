7b946cff672788214fd89a5261dba4a1d255aa09
AshwinSaji10 <aswinsaji777@gmail.com>
2024-09-19 19:45:21 +0530

    added instructor model

---
+++ lib/models/instructors.dart
@@ -0,0 +1,13 @@
+class Instructors {
+  String? iid;
+  String? iname;
+  String? gender;
+  int? age;
+
+  Instructors({
+    required this.iid,
+    required this.iname,
+    required this.gender,
+    required this.age,
+  });
+}
