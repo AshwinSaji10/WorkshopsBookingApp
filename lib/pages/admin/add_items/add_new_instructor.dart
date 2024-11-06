e243da3afb47fd67dbca2308973b899b7bb59285
AshwinSaji10 <aswinsaji777@gmail.com>
2024-11-06 14:33:53 +0530

    drop down menu for gender added

--- lib/pages/admin/add_items/add_new_instructor.dart
+++ lib/pages/admin/add_items/add_new_instructor.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:workshops_booking/services/database_service.dart';
 import 'package:workshops_booking/widgets/form_container_widget.dart';
+import 'package:dropdown_button2/dropdown_button2.dart';
 
 class AddNewInstructor extends StatefulWidget {
   const AddNewInstructor({super.key});
@@ -13,16 +14,19 @@ class AddNewInstructor extends StatefulWidget {
 class _AddNewInstructorState extends State<AddNewInstructor> {
   final TextEditingController _idController = TextEditingController();
   final TextEditingController _nameController = TextEditingController();
-  final TextEditingController _genderController = TextEditingController();
+  // final TextEditingController _genderController = TextEditingController();
   final TextEditingController _ageController = TextEditingController();
 
   final DatabaseService _dbService = DatabaseService.instance;
+
+  final List<String> gender = ['Male', 'Female', 'Other'];
+  String? selectedGender;
 
   @override
   void dispose() {
     _idController.dispose();
     _nameController.dispose();
-    _genderController.dispose();
+    // _genderController.dispose();
     _ageController.dispose();
     super.dispose();
   }
@@ -50,10 +54,100 @@ class _AddNewInstructorState extends State<AddNewInstructor> {
               isPasswordField: false,
             ),
             const SizedBox(height: 20),
-            FormContainerWidget(
-              hintText: "Gender",
-              controller: _genderController,
-              isPasswordField: false,
+            // FormContainerWidget(
+            //   hintText: "Gender",
+            //   controller: _genderController,
+            //   isPasswordField: false,
+            DropdownButtonHideUnderline(
+              child: DropdownButton2<String>(
+                isExpanded: true,
+                hint: const Row(
+                  children: [
+                    Icon(
+                      Icons.list,
+                      size: 16,
+                      color: Colors.black,
+                    ),
+                    SizedBox(
+                      width: 4,
+                    ),
+                    Expanded(
+                      child: Text(
+                        'Select Gender',
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                  ],
+                ),
+                items: gender
+                    .map(
+                      (String item) => DropdownMenuItem<String>(
+                        value: item,
+                        child: Text(
+                          item,
+                          style: const TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          ),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                    )
+                    .toList(),
+                value: selectedGender,
+                onChanged: (value) {
+                  setState(
+                    () {
+                      selectedGender = value;
+                    },
+                  );
+                },
+                buttonStyleData: ButtonStyleData(
+                  height: 60,
+                  width: double.infinity,
+                  padding: const EdgeInsets.only(left: 14, right: 14),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(14),
+                    border: Border.all(
+                      color: Colors.black26,
+                    ),
+                    // color: Colors.redAccent,
+                  ),
+                  elevation: 0,
+                ),
+                iconStyleData: const IconStyleData(
+                  icon: Icon(
+                    Icons.arrow_forward_ios_outlined,
+                  ),
+                  iconSize: 14,
+                  iconEnabledColor: Colors.black,
+                  iconDisabledColor: Colors.grey,
+                ),
+                dropdownStyleData: DropdownStyleData(
+                  maxHeight: 200,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(14),
+                    color: Colors.white,
+                  ),
+                  offset: const Offset(0, 0),
+                  scrollbarTheme: ScrollbarThemeData(
+                    radius: const Radius.circular(40),
+                    thickness: WidgetStateProperty.all(6),
+                    thumbVisibility: WidgetStateProperty.all(true),
+                  ),
+                ),
+                menuItemStyleData: const MenuItemStyleData(
+                  height: 40,
+                  padding: EdgeInsets.only(left: 14, right: 14),
+                ),
+              ),
             ),
             const SizedBox(height: 20),
             FormContainerWidget(
@@ -67,7 +161,7 @@ class _AddNewInstructorState extends State<AddNewInstructor> {
               onPressed: () async {
                 if (_idController.text.isEmpty ||
                     _nameController.text.isEmpty ||
-                    _genderController.text.isEmpty ||
+                    selectedGender == null ||
                     _ageController.text.isEmpty) {
                   Fluttertoast.showToast(
                     msg: "Empty fields",
@@ -82,7 +176,7 @@ class _AddNewInstructorState extends State<AddNewInstructor> {
                     await _dbService.addInstructor(
                       _idController.text,
                       _nameController.text,
-                      _genderController.text,
+                      selectedGender!,
                       int.parse(_ageController.text),
                     );
                     Fluttertoast.showToast(
@@ -97,7 +191,7 @@ class _AddNewInstructorState extends State<AddNewInstructor> {
                     setState(() {
                       _idController.clear();
                       _nameController.clear();
-                      _genderController.clear();
+                      selectedGender = null;
                       _ageController.clear();
                     });
                   } catch (e) {
